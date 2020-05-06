ea6d56efefc863152fbdc67235a5f853fe3fc1b9
isabella.mattua <isa_mattua@hotmail.com>
2020-05-05 21:36:52 -0400

    Finished Part

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './style/errorSass.scss';
+import erroOfLife from '../img/demon_jorge.jpg';
  
 const Error = () => {
     return (
-       <div>
-          <p>Error: Page does not exist!</p>
-       </div>
+      <div class='body'>
+         <div class="box"> <br/> <br/>
+          <p class='title'>Error: Page does not exists!</p>
+          <br/> 
+          <img class="imageCarousel" src={erroOfLife} alt ="Top Ten College !"/>  
+           <br/> <br/> <br/> <br/> <br/>
+      </div>
+      <br/> <br/><br/> <br/>
+         
+      </div>
+   
     );
 }
  
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import './style/menuSass.scss';
- 
 import { NavLink } from 'react-router-dom';
+import logo from '../img/menu-logo.png';
+import './style/menuSass.scss';
+
+ 
+
  
 const Menu = () => {
     return (
        <div class="topnav" id="myTopnav">
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/Home">Home</NavLink>
           <NavLink to="/">Student -</NavLink>
-          <NavLink to="/">Test</NavLink>
+          <NavLink to="/"> <img class="logoImage" src={logo}alt="Menu Logo"/> </NavLink>
           <NavLink to="/form">Form</NavLink>
           <NavLink to="/Pathway">Pathway</NavLink>
        </div>
+       
     );
 }
  
